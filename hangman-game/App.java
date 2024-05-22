f4f62f9b2344003228769b060482b55a561151a0
zach <zacharymd14@gmail.com>
2024-05-22 11:44:38 +0200

    re added the app.java file

---
+++ hangman-game/App.java
@@ -0,0 +1,16 @@
+package com.hangmanegame;
+
+/**
+ * Main file
+ *
+ */
+public class App {
+	public static void main(String[] args) {
+
+		// create the list of word that can be used for the game
+		String[] wordList = { "doggo", "pineapple", "table", "history", "poney" };
+
+		System.out.println(wordList[1].length());
+
+	}
+}
