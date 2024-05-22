0d26edbad9105ffc7969bcf15a91d292c9eb4431
zach <zacharymd14@gmail.com>
2024-05-22 11:56:46 +0200

    added wordlist

--- hangman-game/App.java
+++ hangman-game/App.java
@@ -8,7 +8,13 @@ public class App {
 		// create the list of word that can be used for the game
 		String[] wordList = { "doggo", "pineapple", "table", "history", "poney" };
 
-		System.out.println(wordList[1].length());
+		// Create a random int that is containe in [ 0 ; number of words in the list ]
+		int randomWord = (int) (Math.random() * wordList.length);
+
+		// test purpose, print the lenght of the choosen random word then print the word
+		// itself
+		System.out.println(wordList[randomWord].length());
+		System.out.println(wordList[randomWord]);
 
 	}
 }
